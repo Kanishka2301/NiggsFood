74de61aae5082b9619756e8d2703b105504b9ad4
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-10-04 12:08:57 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={assets.logs} alt="" className="logo" />
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
         <li>home</li>
         <li>menu</li>
